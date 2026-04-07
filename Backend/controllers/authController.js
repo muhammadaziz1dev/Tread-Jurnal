1c7780768f54b42220f155410b832ab18a0e2ae7
muhammadaziz1dev <boburakaoshnasi@gmail.com>
2026-04-07 17:11:58 +0500

    email xat yuborish qoshildi

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -2,6 +2,17 @@ const User = require('../models/User');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
+const nodemailer = require('nodemailer');
+
+// Email sozlamalari
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
+    }
+});
+
 // RO'YXATDAN O'TISH (Register)
 exports.register = async (req, res) => {
     try {
